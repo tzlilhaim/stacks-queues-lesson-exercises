7e05d2fcb242b909d8a8e8458b773376f1f4698c
danbrud <dannybrudner@users.noreply.github.com>
2019-09-16 09:01:35 +0300

    wrote test for isEmpty method

--- tests/exercises/ex-1.test.js
+++ tests/exercises/ex-1.test.js
@@ -1,7 +1,7 @@
 const Stack = require('../../src/stack')
 
 describe('Exercise 1', () => {
-    it('Should create a push method which adds a new element to the top of the stack', function () {
+    it('Should create a `push` method which adds a new element to the top of the stack', function () {
         const stack = new Stack()
         stack.push(1)
 
@@ -11,7 +11,7 @@ describe('Exercise 1', () => {
         expect(stack.stack, `when pushing 1 to the stack, the stack should look like this - [1]`).toEqual(expectedStack)
     })
 
-    it('Should create a peek method which returns the top element of the stack', function () {
+    it('Should create a `peek` method which returns the top element of the stack', function () {
         const stack = new Stack()
 
         for (let i = 1; i <= 3; i++) {
@@ -20,8 +20,9 @@ describe('Exercise 1', () => {
         }
     })
 
-    it('Should create a pop method which removes the top element of the stack (and returns it)', function () {
+    it('Should create a `pop` method which removes the top element of the stack (and returns it)', function () {
         const stack = new Stack()
+        expect(stack.peek(), `when running the 'peek' method on an empty stack the method should return 'null'`).toBe(null)
 
         stack.push(1)
         stack.push(2)
@@ -33,5 +34,22 @@ describe('Exercise 1', () => {
             stack.pop()
             expect(stack.peek(), `when pushing 1,2,3 to the stack and popping, the popped element should be ${i - 1}`).toBe(i - 1)
         }
+    })
+
+    it('Should create an `isEmpty` method which returns `true` if teh stack is empty and `false` otherwise', function () {
+        const stack = new Stack()
+        expect(stack.isEmpty(), `when instanciating a new stack, the 'isEmpty' method should return 'true'`).toBe(true)
+
+        for (let i = 1; i <= 3; i++) {
+            stack.push(i)
+        }
+
+        expect(stack.isEmpty(), `when instanciating a new stack and pushing some values, the 'isEmpty' method should return false`).toBe(false)
+
+        for (let i = 1; i <= 3; i++) {
+            stack.pop(i)
+        }
+
+        expect(stack.isEmpty(), `when instanciating a new stack, the 'isEmpty' method should return 'true'`).toBe(true)
     })
 })
