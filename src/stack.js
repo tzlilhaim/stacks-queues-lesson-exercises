82c42ac68f4f3331670771361afd42f8df2ec7d8
danbrud <dannybrudner@users.noreply.github.com>
2019-09-16 09:02:40 +0300

    removed testing code

--- src/stack.js
+++ src/stack.js
@@ -37,19 +37,9 @@ class Stack {
   }
 }
 
-//You can use the following code to test your solution
-let myStack = new Stack()
-console.log(myStack.isEmpty())    //true
-myStack.print()                    //[]
-myStack.push(2)
-console.log(myStack.isEmpty())     //false
-myStack.push(4)
-myStack.print()                    //[2,4]
-console.log(myStack.peek())        //4
-myStack.pop()
-myStack.pop()
-console.log(myStack.peek())       //null
-console.log(myStack.isEmpty())    //true
+
+//You can paste the code from the lesson below to test your solution
+
 
 
 
