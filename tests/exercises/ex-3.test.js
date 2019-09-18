913c0635d3d39775501ddb47a045acd0668e04c2
danbrud <dannybrudner@users.noreply.github.com>
2019-09-18 12:00:52 +0300

    added tests for DuoQueue

---
+++ tests/exercises/ex-3.test.js
@@ -0,0 +1,33 @@
+const DuoQueue = require('../../src/DuoQueue')
+
+describe('Exercise 3', () => {
+  it('Should create a DuoQueue which consists of ', function () {
+    const dq = new DuoQueue()
+
+    dq.enqueue(1, 1)
+    dq.enqueue(1, 1)
+    dq.enqueue(1, 1)
+    dq.enqueue(2, 2)
+    dq.enqueue(2, 2)
+    dq.dequeue(1)
+    dq.dequeue(1)
+
+    expect(JSON.stringify(dq.getLongestQueue()), '').toContain('[2,2]')
+    expect(JSON.stringify(dq.getShortestQueue()), '').toContain('[1]')
+
+
+    dq.enqueue(1, 1)
+    dq.enqueue(1, 1)
+    dq.enqueue(1, 1)
+    dq.enqueue(1, 1)
+    dq.enqueue(1, 1)
+    dq.enqueue(1, 1)
+
+    expect(JSON.stringify(dq.getLongestQueue()), '').toContain('[1,1,1,1,1,1,1]')
+    expect(JSON.stringify(dq.getShortestQueue()), '').toContain('[2,2]')
+
+    dq.balanceQueues()
+    expect(JSON.stringify(dq.getLongestQueue()), '').toContain('[1,1,1,1,1]')
+    expect(JSON.stringify(dq.getShortestQueue()), '').toContain('[2,2,1,1]')
+  })
+})
