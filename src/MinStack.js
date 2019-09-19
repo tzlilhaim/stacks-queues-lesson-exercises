20c3df7d5cb88f5c1bf9f95ae7c1beb5a4db8dc8
danbrud <dannybrudner@users.noreply.github.com>
2019-09-19 16:26:44 +0300

    removed classes [ci skip]

--- src/MinStack.js
+++ src/MinStack.js
@@ -1,42 +1,13 @@
-const Stack = require('./Stack')
-
-class MinStack extends Stack {
-  constructor() {
-    //Write your code here
-    super()
-    this.minValues = new Stack()
-  }
-
-  push(x) {
-    //Write your code here
-    if (this.isEmpty()) {
-      this.minValues.push(x)
-    } else {
-      x < this.minValues.peek() ? this.minValues.push(x) : null
-    }
-
-    super.push(x)
-  }
-
-  pop() {
-    //Write your code here
-    if (this.isEmpty()) {
-      return null
-    } else {
-      this.length--
-      this.stack[this.length] === this.minValues.peek() ? this.minValues.pop() : null
-      return delete this.stack[this.length]
-    }
-  }
-
-  getMin() {
-    //Write your code here
-    return this.minValues.peek() ? this.minValues.peek() : null
-  }
-}
+/* Write your class below */
 
 
-//You can paste the code from the lesson below to test your solution
+
+
+
+
+
+
+//You can paste the test code from the lesson below to test your solution
 
 
 
--- src/stack.js
+++ src/stack.js
@@ -1,44 +1,13 @@
-
-class Stack {
-  constructor() {
-    this.stack = []
-    this.length = 0
-  }
-
-  push(x) {
-    //Write your code here
-    this.stack[this.length] = x
-    this.length++
-  }
-
-  isEmpty() {
-    //Write your code here
-    return !this.length ? true : false
-  }
-
-  peek() {
-    //Write your code here
-    return this.isEmpty() ? null : this.stack[this.length - 1]
-  }
-
-  pop() {
-    //Write your code here
-    if (this.isEmpty()) {
-      return null
-    } else {
-      this.length--
-      return delete this.stack[this.length]
-    }
-  }
-
-  print() {
-    //Write your code here
-    console.log(this.stack)
-  }
-}
+/* Write your class below */
 
 
-//You can paste the code from the lesson below to test your solution
+
+
+
+
+
+
+//You can paste the test code from the lesson below to test your solution
 
 
 
