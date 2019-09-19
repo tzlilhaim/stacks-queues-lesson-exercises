f697cbe4432a20b093397fe2eda3309a96133262
danbrud <dannybrudner@users.noreply.github.com>
2019-09-19 15:30:07 +0300

    added test for time complexity in DuoQueue

--- tests/exercises/ex-3.test.js
+++ tests/exercises/ex-3.test.js
@@ -1,5 +1,25 @@
 const DuoQueue = require('../../src/DuoQueue')
 
+const enqueueMany = function (duoQueue, addToFirst, addToSecond) {
+
+  for (let i = 0; i < addToFirst; i++) {
+    duoQueue.enqueue(1, 1)
+  }
+
+  for (let i = 0; i < addToSecond; i++) {
+    duoQueue.enqueue(2, 2)
+  }
+}
+
+const getRunTime = function (duoQueue, method) {
+  const startTime = new Date()
+  duoQueue[method]()
+  const endTime = new Date()
+
+  return endTime - startTime
+}
+
+
 describe('Exercise 3', () => {
   it('Should create a DuoQueue which consists of ', function () {
     const dq = new DuoQueue()
@@ -30,5 +50,24 @@ describe('Exercise 3', () => {
 
     expect(JSON.stringify(dq.getLongestQueue()), `after balancing queue - longest queue should be [1,1,1,1,1] but got ${dq.getLongestQueue()}`).toContain('[1,1,1,1,1]')
     expect(JSON.stringify(dq.getShortestQueue()), `after balancing queue - shortest queue should be [2,2,1,1] but got ${dq.getShortestQueue()}`).toContain('[2,2,1,1]')
+  })
+
+  it("The balanceQueue method should run in O(1)", function () {
+    const smallQueue = new DuoQueue()
+    const largeQueue = new DuoQueue()
+
+    enqueueMany(smallQueue, 2500, 7500)
+    enqueueMany(largeQueue, 2500000, 7500000)
+
+    const runTimeShortSmall = getRunTime(smallQueue, "getShortestQueue")
+    const runTimeLongSmall = getRunTime(smallQueue, "getLongestQueue")
+    const runTimeShortLarge = getRunTime(largeQueue, "getShortestQueue")
+    const runTimeLongLarge = getRunTime(largeQueue, "getLongestQueue")
+
+
+    const runTimeDifferenceShortest = Math.abs(runTimeShortLarge - runTimeShortSmall)
+    const runTimeDifferenceLongest = Math.abs(runTimeLongLarge - runTimeLongSmall)
+    expect(runTimeDifferenceShortest, 'the `getShortestQueue` method does not run in O(1)').toBeLessThan(100)
+    expect(runTimeDifferenceLongest, 'the `getLongestQueue` method does not run in O(1)').toBeLessThan(100)
   })
 })
